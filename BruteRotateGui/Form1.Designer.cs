6b6c69b7fd9dd1ce269cb9d841d0cd5461b83293
rizretro <alxjacobs14@gmail.com>
2018-04-11 14:49:23 -0600

    Added main functionality of program.

--- BruteRotateGui/Form1.Designer.cs
+++ BruteRotateGui/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.userText = new System.Windows.Forms.TextBox();
+            this.rotations = new System.Windows.Forms.NumericUpDown();
+            this.characterOutput = new System.Windows.Forms.TextBox();
+            this.primeOutput = new System.Windows.Forms.TextBox();
+            this.rotateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.rotations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userText
+            // 
+            this.userText.Location = new System.Drawing.Point(12, 12);
+            this.userText.Multiline = true;
+            this.userText.Name = "userText";
+            this.userText.Size = new System.Drawing.Size(776, 202);
+            this.userText.TabIndex = 0;
+            // 
+            // rotations
+            // 
+            this.rotations.Location = new System.Drawing.Point(12, 220);
+            this.rotations.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.rotations.Name = "rotations";
+            this.rotations.Size = new System.Drawing.Size(371, 20);
+            this.rotations.TabIndex = 1;
+            // 
+            // characterOutput
+            // 
+            this.characterOutput.Location = new System.Drawing.Point(12, 246);
+            this.characterOutput.Multiline = true;
+            this.characterOutput.Name = "characterOutput";
+            this.characterOutput.ReadOnly = true;
+            this.characterOutput.Size = new System.Drawing.Size(776, 202);
+            this.characterOutput.TabIndex = 2;
+            // 
+            // primeOutput
+            // 
+            this.primeOutput.Location = new System.Drawing.Point(12, 454);
+            this.primeOutput.Multiline = true;
+            this.primeOutput.Name = "primeOutput";
+            this.primeOutput.ReadOnly = true;
+            this.primeOutput.Size = new System.Drawing.Size(776, 202);
+            this.primeOutput.TabIndex = 3;
+            // 
+            // rotateButton
+            // 
+            this.rotateButton.Location = new System.Drawing.Point(389, 220);
+            this.rotateButton.Name = "rotateButton";
+            this.rotateButton.Size = new System.Drawing.Size(399, 20);
+            this.rotateButton.TabIndex = 4;
+            this.rotateButton.Text = "Rotate";
+            this.rotateButton.UseVisualStyleBackColor = true;
+            this.rotateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 667);
+            this.Controls.Add(this.rotateButton);
+            this.Controls.Add(this.primeOutput);
+            this.Controls.Add(this.characterOutput);
+            this.Controls.Add(this.rotations);
+            this.Controls.Add(this.userText);
+            this.Name = "Form1";
+            this.Text = "BruteRotateGUI";
+            ((System.ComponentModel.ISupportInitialize)(this.rotations)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox userText;
+        private System.Windows.Forms.NumericUpDown rotations;
+        private System.Windows.Forms.TextBox characterOutput;
+        private System.Windows.Forms.TextBox primeOutput;
+        private System.Windows.Forms.Button rotateButton;
     }
 }
 
--- BruteRotateGui/Form1.cs
+++ BruteRotateGui/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,56 @@ namespace BruteRotateGui
 {
     public partial class Form1 : Form
     {
+        string[] characters = { "F", "U", "TH", "O", "R", "C(K)", "G", "W", "G", "N", "I", "J", "EO", "P", "X", "S(Z)", "T", "B", "E", "M", "L", "NG(ING)", "OE", "D", "A", "AE", "Y", "IA(IO)", "EA" };
+        int[] primes = { 2, 3, 4, 5, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109 };
+        string ui;
+        string[] uch;
+        int index;
+
+        int rotate = 0;
+        string output = "";
+        string primeOut = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            output = "";
+            primeOut = "";
+            ui = userText.Text;
+            ui = ui.ToUpper();
+            uch = Regex.Split(ui, @"\.|(\s)");
+
+            rotate = (int)rotations.Value;
+
+            foreach(string x in uch) {
+                index = Array.IndexOf(characters, x);
+
+                if(Regex.IsMatch(x, @"\s"))
+                {
+                    output += x;
+                    primeOut += x;
+                    continue;
+                }
+
+                if (index + rotate >= characters.Length) {
+                    output += characters[index + (rotate - characters.Length)];
+                    primeOut += primes[index + (rotate - characters.Length)];
+                    primeOut += "_";
+                }
+                else
+                {
+                    output += characters[index + rotate];
+                    primeOut += primes[index + rotate];
+                    primeOut += " ";
+                }
+            }
+
+            characterOutput.Text = output;
+            primeOutput.Text = primeOut;
         }
     }
 }
